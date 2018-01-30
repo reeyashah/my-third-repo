e2c749815adf8683452c8378b1baf618f917cfc6
Reeya Shah <reeyashah@0587343634.wireless.umich.net>
2018-01-30 09:51:41 -0500

    March

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 print("my name is reeya")
 
-<<<<<<< HEAD
 
-print("whatsup")
-=======
-print("my name is Jean")
->>>>>>> 3d8a4aa0c199d8b1aaa592938e5747fe23186d17
+
+print("March")
+print("my favorite food is noodles")
+print("my favorite type of music is rap")
